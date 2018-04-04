54a721e8f2e4160de0100b305891549e56c30bd9
Parikh Dhruvin S <n01150847@nl-00115-013.mediastudies.ca>
2018-04-04 10:32:46 -0400

    Fourth VC added

--- w2WebView/ViewController.swift
+++ w2WebView/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var webView: UIWebView?
+    @IBOutlet weak var activity: UIActivityIndicatorView?
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        activity?.startAnimating()
+        activity?.isHidden = false;
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        activity?.stopAnimating()
+        activity?.isHidden = true;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlAddress = URL(string: "https://www.humber.ca")
+        let url = URLRequest(url: urlAddress!)
+        webView?.loadRequest(url as URLRequest)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
